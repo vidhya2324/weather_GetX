f94ae49fd7e5e84eae58138150f5f1814fe3b192
VidhyaAnu <vidhyabtech22@gmail.com>
2025-10-06 12:12:04 +0530

    update the code

--- lib/main.dart
+++ lib/main.dart
@@ -103,7 +103,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/splash',
+      initialRoute: '/weather',
       getPages: appRoutes(),
       
     );
